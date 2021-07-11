a29b5bc1164cb9fc782c8bfde7706a2efcc92896
student <student@ip-172-31-62-103.us-west-2.compute.internal>
2021-07-10 17:57:13 -0700

    Continued GamePanel

--- src/GamePanel.java
+++ src/GamePanel.java
@@ -8,9 +8,23 @@ import javax.swing.JPanel;
 public class GamePanel extends JPanel implements ActionListener, KeyListener {
 
 	@Override
-	public void keyPressed(KeyEvent arg0) {
+	public void keyPressed(KeyEvent e) {
 		// TODO Auto-generated method stub
-		
+		if(e.getKeyCode()==KeyEvent.VK_W || e.getKeyCode()==KeyEvent.VK_SPACE || e.getKeyCode()==KeyEvent.VK_UP) {
+			System.out.println("JUMP");
+		}
+		if(e.getKeyCode()==KeyEvent.VK_S || e.getKeyCode()==KeyEvent.VK_CONTROL || e.getKeyCode()==KeyEvent.VK_DOWN) {
+			System.out.println("DUCK");
+		}
+		if(e.getKeyCode()==KeyEvent.VK_A || e.getKeyCode()==KeyEvent.VK_LEFT) {
+			System.out.println("MOVE LEFT");
+		}
+		if(e.getKeyCode()==KeyEvent.VK_D || e.getKeyCode()==KeyEvent.VK_RIGHT) {
+			System.out.println("MOVE RIGHT");
+		}
+		if(e.getKeyCode()==KeyEvent.VK_SHIFT || e.getKeyCode()==KeyEvent.VK_E) {
+			System.out.println("TOGGLE SPRINT");
+		}
 	}
 
 	@Override
