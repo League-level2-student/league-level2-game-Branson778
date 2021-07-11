4950d55629cf009c2391d41e96aa54b7c4012c79
student <student@ip-172-31-62-103.us-west-2.compute.internal>
2021-07-10 17:38:04 -0700

    Starting GamePanel and Runner

--- src/PrecariousPlatformsRunner.java
+++ src/PrecariousPlatformsRunner.java
@@ -1,4 +1,21 @@
+import javax.swing.JFrame;
 
 public class PrecariousPlatformsRunner {
+	JFrame frame = new JFrame();
+	public static final int WIDTH = 800;
+	public static final int LENGTH = 500;
+	GamePanel gamePanel = new GamePanel();
+	
+	public static void main(String[] args) {
+PrecariousPlatformsRunner ppr = new PrecariousPlatformsRunner();
+ppr.setup();
+	}
 
+	void setup() {
+		frame.add(gamePanel);
+		frame.setSize(WIDTH,LENGTH);
+		frame.setVisible(true);
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		frame.addKeyListener(gamePanel);
+	}
 }
