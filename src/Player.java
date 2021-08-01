b41fce3edb35a03d553570533da2bd431be10c88
student <student@ip-172-31-48-167.us-west-2.compute.internal>
2021-08-01 14:51:51 -0700

    Added Player Sprite. No Changes to Jumping Or Ducking.

--- src/Player.java
+++ src/Player.java
@@ -1,11 +1,20 @@
 import java.awt.Color;
 import java.awt.Graphics;
+import java.awt.image.BufferedImage;
+
+import javax.imageio.ImageIO;
 
 public class Player extends MovingObject {
  int jump = 7;
+	public static BufferedImage image;
+	public static boolean needImage = true;
+	public static boolean gotImage = false;	
 	Player(int x, int y, int width, int height) {
 		super(x, y, width, height);
 		// TODO Auto-generated constructor stub
+		if(needImage) {
+			loadImage("player.png");
+		}
 		speed = 12;
 		x=50;
 		y=50;
@@ -13,8 +22,13 @@ public class Player extends MovingObject {
 		height=50;
 	}
 	void draw (Graphics g) {
+		if(gotImage) {
+			g.drawImage(image, x, y, width, height, null);
+		}
+		else {
 		g.setColor(Color.RED);
 		g.fillRect(x, y, width, height);
+		}
 	}
  public void walkLeft() {
 	 x-=speed;
@@ -51,5 +65,16 @@ public class Player extends MovingObject {
 public void update() {
 	
 	
+}
+void loadImage(String imageFile) {
+    if (needImage) {
+        try {
+            image = ImageIO.read(this.getClass().getResourceAsStream(imageFile));
+	    gotImage = true;
+        } catch (Exception e) {
+            
+        }
+        needImage = false;
+    }
 }
 }
