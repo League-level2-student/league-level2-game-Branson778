a92cfd7b22cc7a6b968427402143f79b20cabaa6
student <student@ip-172-31-61-183.us-west-2.compute.internal>
2021-09-04 17:55:36 -0700

    Finish Arrow Dispensing

--- src/ArrowDispenser.java
+++ src/ArrowDispenser.java
@@ -6,8 +6,15 @@ ArrowDispenser(int x, int y, int width, int height) {
 		super(x, y, width, height);
 		// TODO Auto-generated constructor stub
 	}
-
+public boolean facingLeft = false;
+public boolean facingRight = false;
+public Arrow getArrow() {
+	return new Arrow(x, y+height/2, 34, 9 );
+}
 void draw(Graphics g) {
+	
+}
+void fireArrow() {
 	
 }
 }
--- src/MovingObjectManager.java
+++ src/MovingObjectManager.java
@@ -1,11 +1,18 @@
 import java.awt.Color;
 import java.awt.Graphics;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 import java.util.ArrayList;
 
-public class MovingObjectManager {
+import javax.swing.Timer;
+
+public class MovingObjectManager implements ActionListener {
 Player player2 = new Player(50, 400, 50, 50);
 ArrayList<Arrow> arrows = new ArrayList<Arrow>();
+ArrayList<ArrowDispenser> dispensers = new ArrayList<ArrowDispenser>();
+Timer arrowFire = new Timer(1250, this);
 int debugInt;
+
 void  draw (Graphics h) {
 	player2.draw(h);
 	for (int i = 0; i < arrows.size(); i++) {
@@ -22,5 +29,17 @@ void update(){
 	for (int i = 0; i < arrows.size(); i++) {
 		arrows.get(i).update();
 	}
+}
+void startObjects() {
+	arrowFire.start();
+	dispensers.add(new ArrowDispenser(500,300,50,50));
+}
+void addArrow(Arrow arrow) {
+	arrows.add(arrow);
+}
+@Override
+public void actionPerformed(ActionEvent arg0) {
+	// TODO Auto-generated method stub
+	addArrow(dispensers.get(0).getArrow());
 }
 }
