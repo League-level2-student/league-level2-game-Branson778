3846f245a9fd1ee04f552c644db672b1aa639740
student <student@ip-172-31-61-141.us-west-2.compute.internal>
2021-09-11 17:59:45 -0700

    Added Arrow Dispenser Textures

--- src/ArrowDispenser.java
+++ src/ArrowDispenser.java
@@ -1,12 +1,28 @@
 import java.awt.Color;
 import java.awt.Graphics;
+import java.awt.image.BufferedImage;
+
+import javax.imageio.ImageIO;
 
 public class ArrowDispenser extends MovingObject {
-
+	public  BufferedImage image;
+	public  boolean needImage = true;
+	public  boolean gotImage = false;
 ArrowDispenser(int x, int y, int width, int height, boolean facingRight) {
 		super(x, y, width, height);
 		// TODO Auto-generated constructor stub
 		this.facingRight=facingRight;
+		if(needImage) {
+			if(facingRight==true) {
+				loadImage("DispenserRight.png");
+			}
+			else {
+				loadImage("DispenserLeft.png");
+			}
+			//if(facingLeft==false && facingRight==false) {
+				//loadImage("gameBackground.png");
+			//}
+		}
 	}
 //public boolean facingLeft = false;
 public boolean facingRight = false;
@@ -14,8 +30,12 @@ public Arrow getArrow() {
 	return new Arrow(x, y+height/2-12, 34, 9 ,facingRight );
 }
 void draw(Graphics g) {
-	g.setColor(Color.green);
-	g.fillRect(x,y,width,height);
+	if (gotImage) {
+		g.drawImage(image, x, y, width, height, null);
+	} else {
+		g.setColor(Color.GREEN);
+		g.fillRect(x, y, width, height);
+	}
 }
 /*
 void arrowLeft(Arrow arrow) {
@@ -27,4 +47,15 @@ void arrowRight(Arrow arrow) {
 	arrow.facingRight = true;
 }
 */
+void loadImage(String imageFile) {
+    if (needImage) {
+        try {
+            image = ImageIO.read(this.getClass().getResourceAsStream(imageFile));
+	    gotImage = true;
+        } catch (Exception e) {
+            
+        }
+        needImage = false;
+    }
+}
 }
