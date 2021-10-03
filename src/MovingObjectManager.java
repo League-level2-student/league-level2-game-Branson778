219546eaa3421744edda9b9466ef64c1dd67e6c1
student <student@ip-172-31-53-175.us-west-2.compute.internal>
2021-10-02 17:36:06 -0700

    Finished arrow/platform collision

--- src/MovingObjectManager.java
+++ src/MovingObjectManager.java
@@ -9,7 +9,7 @@ import javax.swing.Timer;
 
 public class MovingObjectManager implements ActionListener {
 static Player player2 = new Player(50, 350, 50, 50);
-ArrayList<Arrow> arrows = new ArrayList<Arrow>();
+static ArrayList<Arrow> arrows = new ArrayList<Arrow>();
 static ArrayList<PlatformObject> platforms = new ArrayList<PlatformObject>();
 static ArrayList<ArrowDispenser> dispensers = new ArrayList<ArrowDispenser>();
 static ArrayList<DeathBlock> deathBlocks = new ArrayList<DeathBlock>();
@@ -65,6 +65,7 @@ void startObjects() {
 	dispensers.add(new ArrowDispenser(200,400,50,50,true));
 	dispensers.add(new ArrowDispenser(600,300,50,50,false));
 	platforms.add(new PlatformObject(0, 450, 800, 199));
+	platforms.add(new PlatformObject(200,300,50,50));
 	deathBlocks.add(new DeathBlock(150,425,50,25));
 	baddies.add(new Baddie(600,400,50,50,250,600,true));
 	baddies.add(new Baddie(250,400,50,50,250,600,true));
@@ -124,17 +125,33 @@ void checkCollision() {
 			}
 		}
 	}
+	for (int i = 0; i < platforms.size(); i++) {
+		for (int j = 0; j < arrows.size(); j++) {
+			if(platforms.get(i).collisionBox.intersects(arrows.get(j).collisionBox)) {
+				System.out.println("collisionBox intersction/platform arrow");
+				arrows.get(j).isActive = false;
+			}
+		}
+	}
+	for (int i = 0; i < dispensers.size(); i++) {
+		for (int j = 0; j < arrows.size(); j++) {
+			if(dispensers.get(i).collisionBox.intersects(arrows.get(j).collisionBox)) {
+			//	System.out.println("collisionBox intersction/dispenser arrow");
+			//	arrows.get(j).isActive = false;
+			}
+		}
+	}
 }
 public static boolean checkCollisionArea(Rectangle collider) {
 	for (int i = 0; i < dispensers.size(); i++) {
 		if(dispensers.get(i).collisionBox.intersects(collider)) {
-			System.out.println("collisionBox intersection/dispenser");
+			System.out.println("collisionBox intersection/dispenser player");
 			return true;
 		}
 	}
 	for (int i = 0; i < platforms.size(); i++) {
 		if(platforms.get(i).collisionBox.intersects(collider)) {
-			System.out.println("collisionBox intersection/platform");
+			System.out.println("collisionBox intersection/platform player");
 			return true;
 		}
 	}
