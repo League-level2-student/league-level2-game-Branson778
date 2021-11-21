e258570acd6ce51c8237fee2440a0c5bc07a2e15
r_boo <r_boo@192.168.1.69>
2021-11-20 18:05:00 -0800

    Mistakes fixed in egg

--- src/PrecariousPlatformsRunner.java
+++ src/PrecariousPlatformsRunner.java
@@ -21,6 +21,6 @@ public class PrecariousPlatformsRunner {
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		frame.addKeyListener(gamePanel);
 		frame.setTitle("Precarious Platforms");
-        JOptionPane.showMessageDialog(null, "Your feel as your colors fade, someone is controlling the endless fire. Your memories start to slip away from you. But, before fully being consumed, you see what happens to yours friends and family. They become slaves to a single being. \n Shocked you start running forgetting your past and only remember, free them all. You now burn in the colors of light searching for the slaver of your people. Running far enough will bring you there. \n But where in the eternal jungle, the temple of the endless fire is, is a mystery to you. Someone is controlling your people and you must stop them and show them this won’t happen again.");
+        JOptionPane.showMessageDialog(null, "You feel as your colors fade, someone is controlling the endless fire. Your memories start to slip away from you. But, before fully being consumed, you see what happens to yours friends and family. They become slaves to a single being. \n Shocked you start running forgetting your past and only remember, free them all. You now burn in the colors of light searching for the slaver of your people. Running far enough will bring you there. \n But where in the eternal jungle, the temple of the endless fire is, is a mystery to you. Someone is controlling your people and you must stop them and show them this won’t happen again.");
 	}
 }
